9dbb9b93475c814291ddf3c62cf31a2cf48ec597
Kapibarabanka <kapibarabanka@gmail.com>
2021-03-11 22:02:34 +0200

    Add comments for new id field in UrlAlias class

--- src/main/java/edu/kpi/testcourse/dataservice/UrlAlias.java
+++ src/main/java/edu/kpi/testcourse/dataservice/UrlAlias.java
@@ -2,6 +2,8 @@ package edu.kpi.testcourse.dataservice;
 
 /**
  * Stores data about url aliases:
+ *  <p>id - integer id from which alias was generated ot {@code -1} if
+ *  alias was provided by user;</p>
  *  <p>alias - unique key, shortened url;</p>
  *  <p>url - full url;</p>
  *  <p>user - email of the user, that created this alias</p>.
@@ -13,8 +15,9 @@ public class UrlAlias {
   private String user;
 
   /**
-   * Class constructor with all fields filled.
+   * Constructor for objects with automatically generated aliases.
    *
+   * @param id unique integer id from which alias was generated
    * @param alias unique key, short name for the {@code url}
    * @param url full url
    * @param user email of user that created this alias
@@ -27,7 +30,8 @@ public class UrlAlias {
   }
 
   /**
-   * Class constructor with all fields filled.
+   * Constructor for objects with aliases, provided by user.
+   * Id in this objects is equal to {@code -1}
    *
    * @param alias unique key, short name for the {@code url}
    * @param url full url
