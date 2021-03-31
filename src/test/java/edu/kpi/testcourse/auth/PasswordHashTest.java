d9519152178378aab0a7c1ee8c3f31c5a16f8e28
NPashaO <npsha@ukr.net>
2021-03-31 17:27:01 +0300

    Implemented a Random Generate data function and the test of adding user to the DataBase.

--- src/test/java/edu/kpi/testcourse/auth/PasswordHashTest.java
+++ src/test/java/edu/kpi/testcourse/auth/PasswordHashTest.java
@@ -1,9 +1,9 @@
 package edu.kpi.testcourse.auth;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
 
 public class PasswordHashTest {
   @Test
