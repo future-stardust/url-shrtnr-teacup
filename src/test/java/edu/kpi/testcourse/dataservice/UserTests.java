e779d89af541f3e210350e128123931f180ed579
NPashaO <npsha@ukr.net>
2021-04-01 15:48:41 +0300

    Reorganize

--- src/test/java/edu/kpi/testcourse/dataservice/UserTests.java
+++ src/test/java/edu/kpi/testcourse/dataservice/UserTests.java
@@ -46,38 +46,5 @@ public class UserTests extends DataServiceImplTest{
   }
 
 
-  /**
-   * Tests written by the Uncontested group
-   */
 
-  public static String Generator() {
-
-    final String LOWER = "abcdefghijklmnopqrstuvwxyz";
-    final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    final String DIGITS = "0123456789";
-    final String PUNCTUATION = "!@#$%&*()_+-=[]|,./?><";
-    final List<String> charCategories = Arrays.asList((LOWER + UPPER + DIGITS).split(""));
-    int length = 6;
-
-    // Variables.
-    StringBuilder password = new StringBuilder(length);
-    Random random = new Random(System.nanoTime());
-
-    // Build the password.
-    for (int i = 0; i < length; i++) {
-      String charCategory = charCategories.get(random.nextInt(charCategories.size()));
-      int position = random.nextInt(charCategory.length());
-      password.append(charCategory.charAt(position));
-    }
-    return new String(password);
-  }
-
-  @Test
-  void addUserTest() {
-
-    User Pasha = new User(Generator(),Generator() );
-    dataService.addUser(Pasha);
-    System.out.println(Pasha.getPasswordHash() + "\n" + Pasha.getEmail());
-    assertThat(dataService.getUser(Pasha.getEmail())).isNotNull();
-  }
 }
